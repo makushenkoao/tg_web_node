78e5d6054972b14a3961d42c2a4bd0ced1d3dc8a
Anton <108577345+makushenkoao@users.noreply.github.com>
2023-05-15 03:43:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
 
-const token = '6044054585:AAFut3IMQkLTnecgGHhi_n_L161BrycKOEM';
-const webAppUtl = 'https://serene-sherbet-aa7969.netlify.app'
+const token = '';
+const webAppUtl = '
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
 app.use(express.json())
